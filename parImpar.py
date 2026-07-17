a786c48147a1c93deafaf3cfe2db6bd0824113cd
joao77734 <oshymist@gmail.com>
2026-07-17 17:10:08 -0400

    comando ParImpar atualizado com def

--- parImpar.py
+++ parImpar.py
@@ -1,24 +1,25 @@
 #sistema Par ou Ímpar
 import random
 
-print("=== PAR OU ÍMPAR ===")
+def par_ou_impar():
+    print("==== PAR OU ÍMPAR ====")
 
-jogador = input("Escolha Par ou Ímpar (P/I): ").upper()
-numero = int(input("Digite um número de 0 a 10: "))
+    jogador=input("Escolha par ou ímpar (P/I):")
+    numero = int(input("Digite o número de 0 a 10:"))
 
-computador = random.randint(0, 10)
+    computador=random.randint(0,10)
+    soma=numero+computador
 
-soma = numero + computador
+    print("Computador escolheu: ", computador)
+    print("Soma: ", soma)
 
-print("Computador escolheu:", computador)
-print("Soma:", soma)
+    if soma % 2 ==0:
+        resultado = "P"
+    else:
+        resultado = "I"
 
-if soma % 2 == 0:
-    resultado = "P"
-else:
-    resultado = "I"
-
-if jogador == resultado:
-    print("Você ganhou!")
-else:
-    print("Você perdeu!")
+    if jogador==resultado:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+par_ou_impar()
