865622afe80cc37ba995ec5d53ac813693680875
joao77734 <oshymist@gmail.com>
2026-07-17 16:57:39 -0400

    add outro commit

--- juntarArquivos.py
+++ juntarArquivos.py
@@ -8,41 +8,4 @@ opcao = input()
 if opcao == "1":
 
     print("\n=== PAR OU ÍMPAR ===")
-
-    jogador = input("Escolha Par ou Ímpar (P/I): ").upper()
-    numero = int(input("Digite um número de 0 a 10: "))
-
-    computador = random.randint(0, 10)
-
-    soma = numero + computador
-
-    print("Computador escolheu:", computador)
-    print("Soma:", soma)
-
-    if soma % 2 == 0:
-        resultado = "P"
-    else:
-        resultado = "I"
-
-    if jogador == resultado:
-        print("Você ganhou!")
-    else:
-        print("Você perdeu!")
-
-    continuar = input("\nDeseja continuar para ver o próximo comando? (S/N): ").upper()
-
-    if continuar == "N":
-        print("Programa encerrado.")
-    else:
-        print("\nComando do Vinícius:")
-
-        def subtração(n1, n2):
-            result = n1 - n2
-            print("O resultado dos numeros", n1, "e", n2, "é:", result)
-
-        n1 = int(input("Digite um número para subtrair:\n"))
-        n2 = int(input("Digite o segundo número para subtrair:\n"))
-
-        subtração(n1, n2)
-
-        print("\nEspero q tenha gostado!")
+    
