496ad4ab40679aadf0dd171beeefbf6aa2d10739
Vinicius <viniciusspinola82@gmail.com>
2026-07-17 15:57:47 -0400

    commit novo

--- subtração.py
+++ subtração.py
@@ -1,6 +1,6 @@
-def par_ou_impar(numero):
-    numero = int(input("digite um número: \n"))
-    if numero % 2 == 0:
-        return "par"
-    else:
-        return "impar"
+def subtração(n1,n2):
+    result = n1-n2
+    print ("O resultado dos numeros", n1, "e", n2, "é: ", result)
+n1 = int(input("Digite um número para subtrair: \n"))
+n2 = int(input("Digite o segundo número para subtrair: \n"))
+subtração(n1,n2)
